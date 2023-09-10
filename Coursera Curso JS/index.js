f48b2f4aef78965760cd9870a1369258a6d8ab75
emmanuelvp88 <emmanuel.villalvap@gmail.com>
2023-09-10 14:42:04 -0600

    ultomo commit de Coursera

--- Coursera Curso JS/index.js
+++ Coursera Curso JS/index.js
@@ -1,23 +1,19 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic) {
-    elemento = document.getElementById("image");
-    elemento.innerHTML = "alt"
+    let elemento = document.getElementById("image");
     /* In this function you should 
-       1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image
-       
-       2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
-       */
+    1) change the url for the background image of the div with the id = "image"*/
+    elemento.style.backgroundImage = "url(" + previewPic.src + ")";
+    /*2) Change the text  of the div with the id = "image" */
+    elemento.innerHTML = previewPic.alt;
 }
 
 function unDo() {
+    var elemento = document.getElementById("image");
     /* In this function you should 
-       1) Update the url for the background image of the div with the id = "image" 
-       back to the orginal-image.  You can use the css code to see what that original URL was
-       
-       2) Change the text  of the div with the id = "image" 
-       back to the original text.  You can use the html code to see what that original text was
-       */
+    1) Update the url for the background image of the div with the id = "image" */
+    elemento.style.backgroundImage = "";
+    /*2) Change the text  of the div with the id = "image" */
+    elemento.innerHTML = "Hover over an image below to display here";
 }
